e9c7b5da15d901a0ebd216e6e40ad4f45638729c
Daniel Larkin <daniel@larkin.nz>
2022-08-04 19:26:25 +1200

    Update waitforelement.js

--- waitforelement.js
+++ waitforelement.js
@@ -1,19 +1,40 @@
 function waitForElement(selector) {
-    return new Promise(resolve => {
-        if (document.querySelector(selector)) {
-            return resolve(document.querySelector(selector));
-        }
+  return new Promise(resolve => {
+    if (document.querySelector(selector)) {
+      return resolve(document.querySelector(selector));
+    }
 
-        const observer = new MutationObserver(mutations => {
-            if (document.querySelector(selector)) {
-                resolve(document.querySelector(selector));
-                observer.disconnect();
-            }
-        });
-
-        observer.observe(document.body, {
-            childList: true,
-            subtree: true
-        });
+    const observer = new MutationObserver(mutations => {
+      if (document.querySelector(selector)) {
+        resolve(document.querySelector(selector));
+        observer.disconnect();
+      }
     });
+
+    observer.observe(document.body, {
+      childList: true,
+      subtree: true
+    });
+  });
 }
+
+// Select the form
+waitForElement('.form-wrapper').then(form => {
+
+  console.log('form', form)
+
+  // Select the email field
+  const email = form.querySelector('input[name="email"]');
+
+  console.log(email)
+
+  // Define the default redirect url
+  const url = form.getAttribute('data-success-redirect');
+
+  // Handle the email change
+  email.addEventListener('input', () => {
+
+    // Update the url
+    form.setAttribute('data-success-redirect', url + '?email=' + email.value);
+   });
+});
